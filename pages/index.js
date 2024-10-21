ee00d4fc57934fbc13722a7136578714edce7f33
LetThatSinkIn <82845242+LetThatSinkIn@users.noreply.github.com>
2024-10-21 11:47:36 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Fairfax VIP</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Fairfax VIP" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Welcome, page is currently <code>under construction</code>.
         </p>
       </main>
 
